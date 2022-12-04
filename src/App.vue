091b71a0004f4beeaa36a0586ac56305d9f7c01f
Mauran <thomas.mauran@etu.umontpellier.fr>
2022-12-04 14:00:56 +0100

    Working needs css and tts

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Omoikane</h1>
+  <input type="text" v-model="inputText" />
+  <button type="button" @click="fetchResponse">Ask omoikane</button>
+  <button type="button" @click="speech('salut')">speak</button>
+
+  {{ response }}
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from "vue";
+import { Configuration, OpenAIApi } from "openai";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
+
+const configuration = new Configuration({
+  organization: "org-sLV703vpHvgJpm8oikvH1y4m",
+  apiKey: process.env.VUE_APP_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+
+
+let inputText = ref("");
+let response = ref("")
+// let tts
+
+async function fetchResponse() {
+  const gptResponse = await openai.createCompletion(
+    {
+      "model": "text-davinci-003",
+  "prompt": `${inputText.value}`,
+  "max_tokens": 250,
+  "temperature": 0.9,
+  "top_p": 1,
+  "n": 1,
+    },
+
+  );
+  response.value = gptResponse.data.choices[0].text
+  const utterance = new SpeechSynthesisUtterance("Salut je vais bien")
+  utterance.volume = 10
+  utterance.lang = "fr-FR"
+  console.log(speechSynthesis.getVoices())
+  console.log(utterance)
+  speechSynthesis.cancel()
+  speechSynthesis.speak(utterance)
+
 }
+
 </script>
 
 <style>
